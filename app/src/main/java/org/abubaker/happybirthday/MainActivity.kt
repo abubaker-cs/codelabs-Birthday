08329de55c058ab2959fffc01856f2fb4500b0b5
abubaker-cs <mohammad_abubaker@yahoo.com>
2022-02-13 17:08:25 +0500

    Adding image to the birthday screen

---
+++ app/src/main/java/org/abubaker/happybirthday/MainActivity.kt
@@ -0,0 +1,19 @@
+package org.abubaker.happybirthday
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import org.abubaker.happybirthday.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        setContentView(binding.root)
+
+    }
+}
